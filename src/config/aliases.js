c1bcd4178de926e0f8bb278b079e3b8cc5a26eb7
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-01 00:49:56 +0300

    feat: update imports for relative paths import
    	modified:   src/config/aliases.js
    	modified:   src/lib/core/import.js

--- src/config/aliases.js
+++ src/config/aliases.js
@@ -6,14 +6,3 @@ window.ALIASES = {
   '@config': '/src/config',
   '@core': '/src/lib/core'
 };
-
-window.joinPath = function () {
-  var parts = [];
-  for (var i = 0; i < arguments.length; i++) {
-    if (arguments[i]) {
-      var part = arguments[i].replace(/^\/+|\/+$/g, '');
-      if (part) parts.push(part);
-    }
-  }
-  return '/' + parts.join('/');
-};
--- src/lib/core/import.js
+++ src/lib/core/import.js
@@ -2,24 +2,47 @@
 
 var moduleCache = {};
 
-function $import(aliasPath) {
-  if (moduleCache[aliasPath]) {
-    return moduleCache[aliasPath];
+
+function joinPath() {
+  var parts = [];
+  for (var i = 0; i < arguments.length; i++) {
+    if (arguments[i]) {
+      var part = arguments[i].replace(/^\/+|\/+$/g, '');
+
+      if (part.includes('//')) {
+        throw new Error(`Invalid path part: "${arguments[i]}" contains double slashes "//"`);
+      }
+
+      if (part) parts.push(part);
+    }
+  }
+  return '/' + parts.join('/');
+};
+
+
+function resolvePath(basePath, relativePath) {
+  var base = basePath.replace(/\/$/, '');
+  var stack = base.split('/').filter(Boolean);
+
+  var parts = relativePath.split('/').filter(part => part !== '');
+
+  for (var i = 0; i < parts.length; i++) {
+    if (parts[i] === '.') continue;
+    if (parts[i] === '..') {
+      if (stack.length > 0) stack.pop();
+    } else {
+      stack.push(parts[i]);
+    }
   }
 
-  if (!window.ALIASES) {
-    throw new Error('Aliases not loaded. Make sure aliases.js is loaded first');
+  return '/' + stack.join('/');
+}
+
+
+function loadModule(fullPath, requestPath) {
+  if (moduleCache[fullPath]) {
+    return moduleCache[fullPath];
   }
-
-  var parts = aliasPath.split('/');
-  var alias = parts[0];
-  var modulePath = parts.slice(1).join('/');
-
-  if (!window.ALIASES[alias]) {
-    throw new Error('Alias not found: ' + alias + '. Available: ' + Object.keys(window.ALIASES).join(', '));
-  }
-
-  var fullPath = window.joinPath(window.ALIASES[alias], modulePath + '.js');
 
   var xhr = new XMLHttpRequest();
   xhr.open('GET', fullPath, false);
@@ -31,27 +54,70 @@ function $import(aliasPath) {
 
   var moduleExports = {};
   var module = { exports: moduleExports };
+  var moduleDir = fullPath.substring(0, fullPath.lastIndexOf('/'));
 
-  try {
-    var code = '(function(module, exports) {' + xhr.responseText + '\n})(module, module.exports);';
-    eval(code);
-  } catch (error) {
-    throw new Error('Error in module ' + aliasPath + ': ' + error.message);
+  // локальная версия $import для этого модуля
+  function localImport(path) {
+    return $import(path, moduleDir);
   }
 
-  moduleCache[aliasPath] = module.exports;
+  try {
+    var code =
+      '(function(module, exports, __dirname, $import){' +
+      xhr.responseText +
+      '\n})(module, module.exports, "' + moduleDir + '", localImport);';
+    eval(code);
+  } catch (error) {
+    throw new Error('Error in module ' + requestPath + ': ' + error.message);
+  }
+
+  moduleCache[fullPath] = module.exports;
   return module.exports;
 }
 
+
+function $import(requestPath, parentDir) {
+  var fullPath;
+
+  // относительный импорт
+  if (requestPath.startsWith('./') || requestPath.startsWith('../')) {
+    if (!parentDir) {
+      throw new Error('Relative import "' + requestPath + '" used without parent context');
+    }
+    fullPath = resolvePath(parentDir, requestPath);
+  } else {
+    // импорт через алиас
+    if (!window.ALIASES) {
+      throw new Error('Aliases not loaded. Make sure aliases.js is loaded first');
+    }
+    var parts = requestPath.split('/');
+    var alias = parts[0];
+    var modulePath = parts.slice(1).join('/');
+
+    if (!window.ALIASES[alias]) {
+      throw new Error('Alias not found: ' + alias + '. Available: ' + Object.keys(window.ALIASES).join(', '));
+    }
+
+    fullPath = window.joinPath(window.ALIASES[alias], modulePath);
+  }
+
+  // добавляем .js, если расширение отсутствует
+  if (!/\.js$/i.test(fullPath)) {
+    fullPath += '.js';
+  }
+
+  console.log(fullPath, requestPath)
+  return loadModule(fullPath, requestPath);
+}
+
+// --- Алиасы ---
 $import.addAlias = function (alias, path) {
   if (!window.ALIASES) window.ALIASES = {};
   window.ALIASES[alias] = path;
 };
-
 $import.getAlias = function (alias) {
   return window.ALIASES ? window.ALIASES[alias] : undefined;
 };
-
 $import.getAllAliases = function () {
   return window.ALIASES ? Object.assign({}, window.ALIASES) : {};
 };
