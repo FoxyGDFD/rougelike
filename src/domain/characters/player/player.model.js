11feded164283b154d5ae61139dc0a8416ee46ae
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-01 01:30:13 +0300

    fix: class extends and static methods feat: add signals to core feat: write draft mvvm player settings
    	modified:   index.html
    	deleted:    src/domain/characters/player.js
    	new file:   src/domain/characters/player/player.model.js
    	new file:   src/domain/characters/player/player.view-model.js
    	new file:   src/domain/characters/player/player.view.js
    	deleted:    src/domain/characters/unit.js
    	new file:   src/domain/characters/unit.model.js
    	modified:   src/index.js
    	modified:   src/lib/core/class.js
    	new file:   src/lib/core/signal.js

---
+++ src/domain/characters/player/player.model.js
@@ -0,0 +1,40 @@
+var Class = $import('@core/class');
+var Unit = $import('@domain/characters/unit.model');
+var { signal } = $import('@core/signal');
+
+var PlayerModel = Class.create({
+  extends: Unit,
+  constructor: function (stats) {
+    PlayerModel.__super__.constructor.call(this);
+
+    this.gold = signal(stats?.gold || 0);
+    this.inventory = signal(stats?.inventory || []);
+  },
+  methods: {
+    move: function (dx, dy) {
+      this.x.value += dx;
+      this.y.value += dy;
+    },
+    setHealth: function (amount) {
+      if (amount > this.maxHealth) {
+        this.health.value = this.maxHealth;
+      } else {
+        this.health.value = amount
+      }
+    },
+    setGold: function (amount) {
+      this.gold.value = amount;
+    },
+    addItem: function (item) {
+      var arr = this.inventory.value.slice();
+      arr.push(item);
+      this.inventory.value = arr;
+    }
+  }
+});
+
+PlayerModel.createNew = function () {
+  return new PlayerModel(0, 0);
+};
+
+module.exports = PlayerModel;
