12e52ad07f19171f93153e61261e125f486ba746
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-01 17:37:47 +0300

    feat: add eslint and prettier configs

--- src/config/aliases.js
+++ src/config/aliases.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 window.ALIASES = {
   '@domain': '/src/domain',
   '@lib': '/src/lib',
   '@config': '/src/config',
-  '@core': '/src/lib/core'
-};
+  '@core': '/src/lib/core',
+}
--- src/lib/core/import.js
+++ src/lib/core/import.js
@@ -1,143 +1,152 @@
-'use strict';
+'use strict'
 
-var moduleCache = {};
-
+var moduleCache = {}
 
 function joinPath() {
-  var parts = [];
+  var parts = []
   for (var i = 0; i < arguments.length; i++) {
     if (arguments[i]) {
-      var part = arguments[i].replace(/^\/+|\/+$/g, '');
+      var part = arguments[i].replace(/^\/+|\/+$/g, '')
 
       if (part.includes('//')) {
-        throw new Error(`Invalid path part: "${arguments[i]}" contains double slashes "//"`);
+        throw new Error(
+          `Invalid path part: "${arguments[i]}" contains double slashes "//"`
+        )
       }
 
-      if (part) parts.push(part);
+      if (part) parts.push(part)
     }
   }
-  return '/' + parts.join('/');
-};
-
-
-function resolvePath(basePath, relativePath) {
-  var base = basePath.replace(/\/$/, '');
-  var stack = base.split('/').filter(Boolean);
-
-  var parts = relativePath.split('/').filter(part => part !== '');
-
-  for (var i = 0; i < parts.length; i++) {
-    if (parts[i] === '.') continue;
-    if (parts[i] === '..') {
-      if (stack.length > 0) stack.pop();
-    } else {
-      stack.push(parts[i]);
-    }
-  }
-
-  return '/' + stack.join('/');
+  return '/' + parts.join('/')
 }
 
+function resolvePath(basePath, relativePath) {
+  var base = basePath.replace(/\/$/, '')
+  var stack = base.split('/').filter(Boolean)
+
+  var parts = relativePath.split('/').filter((part) => part !== '')
+
+  for (var i = 0; i < parts.length; i++) {
+    if (parts[i] === '.') continue
+    if (parts[i] === '..') {
+      if (stack.length > 0) stack.pop()
+    } else {
+      stack.push(parts[i])
+    }
+  }
+
+  return '/' + stack.join('/')
+}
 
 function loadModule(fullPath, requestPath) {
   if (moduleCache[fullPath]) {
-    return moduleCache[fullPath];
+    return moduleCache[fullPath]
   }
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', fullPath, false);
-  xhr.send();
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', fullPath, false)
+  xhr.send()
 
   if (xhr.status !== 200) {
-    throw new Error('Module not found: ' + fullPath);
+    throw new Error('Module not found: ' + fullPath)
   }
 
-  var moduleExports = {};
-  var module = { exports: moduleExports };
-  var moduleDir = fullPath.substring(0, fullPath.lastIndexOf('/'));
+  var moduleExports = {}
+  var module = { exports: moduleExports }
+  var moduleDir = fullPath.substring(0, fullPath.lastIndexOf('/'))
 
   function localImport(path) {
-    return $import(path, moduleDir);
+    return $import(path, moduleDir)
   }
 
   try {
-    var code = xhr.responseText;
+    var code = xhr.responseText
 
     var moduleFunction = new Function(
-      'module', 'exports', '__dirname', '$import',
-      '\n//# sourceURL=' + fullPath + '\n' +
-      code
-    );
+      'module',
+      'exports',
+      '__dirname',
+      '$import',
+      '\n//# sourceURL=' + fullPath + '\n' + code
+    )
 
-    moduleFunction(module, module.exports, moduleDir, localImport);
+    moduleFunction(module, module.exports, moduleDir, localImport)
   } catch (error) {
-    var errorMessage = 'Error loading module: ' + requestPath +
-      '\nFile: ' + fullPath;
+    var errorMessage =
+      'Error loading module: ' + requestPath + '\nFile: ' + fullPath
 
     if (error.stack) {
-      var stackLines = error.stack.split('\n');
+      var stackLines = error.stack.split('\n')
       for (var i = 0; i < stackLines.length; i++) {
         if (stackLines[i].includes(fullPath)) {
-          errorMessage += '\nLocation: ' + stackLines[i].trim();
-          break;
+          errorMessage += '\nLocation: ' + stackLines[i].trim()
+          break
         }
       }
 
       if (i === stackLines.length) {
-        errorMessage += '\nStack: ' + error.stack;
+        errorMessage += '\nStack: ' + error.stack
       }
     } else {
-      errorMessage += '\nError: ' + error.message;
+      errorMessage += '\nError: ' + error.message
     }
 
-    throw new Error(errorMessage);
+    throw new Error(errorMessage)
   }
 
-  moduleCache[fullPath] = module.exports;
-  return module.exports;
+  moduleCache[fullPath] = module.exports
+  return module.exports
 }
 
-
 function $import(requestPath, parentDir) {
-  var fullPath;
+  var fullPath
 
   if (requestPath.startsWith('./') || requestPath.startsWith('../')) {
     if (!parentDir) {
-      throw new Error('Relative import "' + requestPath + '" used without parent context');
+      throw new Error(
+        'Relative import "' + requestPath + '" used without parent context'
+      )
     }
-    fullPath = resolvePath(parentDir, requestPath);
+    fullPath = resolvePath(parentDir, requestPath)
   } else {
     if (!window.ALIASES) {
-      throw new Error('Aliases not loaded. Make sure aliases.js is loaded first');
+      throw new Error(
+        'Aliases not loaded. Make sure aliases.js is loaded first'
+      )
     }
-    var parts = requestPath.split('/');
-    var alias = parts[0];
-    var modulePath = parts.slice(1).join('/');
+    var parts = requestPath.split('/')
+    var alias = parts[0]
+    var modulePath = parts.slice(1).join('/')
 
     if (!window.ALIASES[alias]) {
-      throw new Error('Alias not found: ' + alias + '. Available: ' + Object.keys(window.ALIASES).join(', '));
+      throw new Error(
+        'Alias not found: ' +
+          alias +
+          '. Available: ' +
+          Object.keys(window.ALIASES).join(', ')
+      )
     }
 
-    fullPath = window.joinPath(window.ALIASES[alias], modulePath);
+    fullPath = window.joinPath(window.ALIASES[alias], modulePath)
   }
 
   if (!/\.js$/i.test(fullPath)) {
-    fullPath += '.js';
+    fullPath += '.js'
   }
 
-  return loadModule(fullPath, requestPath);
+  return loadModule(fullPath, requestPath)
 }
 
 // --- Алиасы ---
 $import.addAlias = function (alias, path) {
-  if (!window.ALIASES) window.ALIASES = {};
-  window.ALIASES[alias] = path;
-};
+  if (!window.ALIASES) window.ALIASES = {}
+  window.ALIASES[alias] = path
+}
 $import.getAlias = function (alias) {
-  return window.ALIASES ? window.ALIASES[alias] : undefined;
-};
+  return window.ALIASES ? window.ALIASES[alias] : undefined
+}
 $import.getAllAliases = function () {
-  return window.ALIASES ? Object.assign({}, window.ALIASES) : {};
-};
+  return window.ALIASES ? Object.assign({}, window.ALIASES) : {}
+}
 
-window.$import = $import;
+window.$import = $import
