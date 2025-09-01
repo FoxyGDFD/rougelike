7696270b524a96dfe695502afe83d660f75d0400
FoxyGDFD <mic.panfil0v@gmail.com>
2025-09-01 14:43:48 +0300

    fix: close model signals in player view model (view react on vm signal, not models)
    	modified:   src/domain/characters/player/player.model.js
    	modified:   src/domain/characters/player/player.view-model.js
    	modified:   src/domain/characters/player/player.view.js
    	modified:   src/domain/characters/unit.model.js

--- src/domain/characters/player/player.view-model.js
+++ src/domain/characters/player/player.view-model.js
@@ -1,26 +1,56 @@
-var Class = $import('@core/class');
-var PlayerModel = $import('./player.model');
+var Class = $import('@core/class')
+var PlayerModel = $import('./player.model')
+var computed = $import('@core/signal').computed
 
 var PlayerViewModel = Class.create({
   constructor: function () {
-    this.__model__ = PlayerModel.createNew();
-
-    this.x = this.__model__.x;
-    this.y = this.__model__.y;
-    this.health = this.__model__.health;
-    this.gold = this.__model__.gold;
-    this.inventory = this.__model__.inventory;
+    this.__model__ = PlayerModel.createNew()
+    this.x = computed(
+      function () {
+        return this.__model__.x.value
+      }.bind(this)
+    )
+    this.y = computed(
+      function () {
+        return this.__model__.y.value
+      }.bind(this)
+    )
+    this.health = computed(
+      function () {
+        return this.__model__.health.value
+      }.bind(this)
+    )
+    this.gold = computed(
+      function () {
+        return this.__model__.gold.value
+      }.bind(this)
+    )
+    this.inventory = computed(
+      function () {
+        return this.__model__.inventory.value.slice()
+      }.bind(this)
+    )
   },
   methods: {
-    move: function (dx, dy) { this.__model__.move(dx, dy); },
-    takeDamage: function (amount) { this.__model__.setHealth(this.health.value - amount); },
-    heal: function (amount) { this.__model__.setHealth(this.health.value + amount); },
-    addGold: function (amount) { this.__model__.setGold(this.gold.value + amount); },
-    addItem: function (item) { this.__model__.addItem(item); }
-  }
-});
+    move: function (dx, dy) {
+      this.__model__.move(dx, dy)
+    },
+    takeDamage: function (amount) {
+      this.__model__.setHealth(this.health.value - amount)
+    },
+    heal: function (amount) {
+      this.__model__.setHealth(this.health.value + amount)
+    },
+    addGold: function (amount) {
+      this.__model__.setGold(this.gold.value + amount)
+    },
+    addItem: function (item) {
+      this.__model__.addItem(item)
+    },
+  },
+})
 PlayerViewModel.createNew = function () {
-  return new PlayerViewModel();
-};
+  return new PlayerViewModel()
+}
 
-module.exports = PlayerViewModel;
+module.exports = PlayerViewModel
